585be7b4f2c99cec508c7c92934c535170aa473f
ritamsarmah <sarmah.ritam@gmail.com>
2020-02-03 20:54:19 -0800

    Fix bug with breaking database

--- src/components/Editor/Marker/ListMarker.js
+++ src/components/Editor/Marker/ListMarker.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Popover from '../Popover/Popover';
 import Tippy from '@tippy.js/react';
-import Marker from './Marker';
+
+import database from '../../../common/Database';
 
 import './Marker.css'
 import 'tippy.js/themes/light-border.css';
 
-export default class ListMarker extends Marker {
+export default class ListMarker extends Component {
+    constructor(props) {
+        super(props);
+        // Fill marker if voice command for the function already exists
+        var command = database.findCommand(this.props.file, this.props.triggerFn);
+
+        this.state = { command: command };
+        this.onClick = this.onClick.bind(this);
+        this.handlePopoverUnmount = this.handlePopoverUnmount.bind(this);
+    }
+
+    onClick() {
+        if (!this.state.isVisible) {
+            this.setState({ isVisible: null });
+        }
+    }
+
+    handlePopoverUnmount() {
+        this.setState({
+            command: null,
+            isVisible: false,
+        });
+    }
+
     render() {
         var content = (this.state.command != null) ? (<Popover command={this.state.command} unmountMe={this.handlePopoverUnmount} />) : (<span></span>);
         var fillClass = (this.state.command != null) ? "filledMarker" : "emptyMarker";
