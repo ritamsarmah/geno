ff4fbd19fb009ac4540097f3838bc151be877794
ritamsarmah <sarmah.ritam@gmail.com>
2020-03-12 23:00:25 -0700

    Update copy

--- src/common/constants.js
+++ src/common/constants.js
@@ -29,7 +29,7 @@ const GenoEvent = {
 
 const HelpText = {
     ExampleQueries: "Example queries are used to trigger a voice command, by matching user's voice input to similar examples. 5 or more examples are recommended for accurate recognition. Select parameters in a query by clicking the query in the list, and using the dropdowns under each word. Click \"Train Model\" to save changes to queries.",
-    Multimodal: "Use multimodal context to infer a parameter's value from hovering or dragging the mouse over elements. To limit context to certain selectors, click the crosshairs and hover over an element, before clicking the crosshairs again to finish. A specific attribute can be selected as a parameter value by hovering over the selector text."
+    Multimodal: "Multimodal context infers a parameter's value from hovering or dragging the mouse over elements. To limit context to certain selectors, click the crosshairs and hover over an element, before clicking the crosshairs again to finish. A specific attribute can be selected as a parameter value by hovering over the selector text."
 }
 
 module.exports = { Colors, Paths, ContextType, GenoEvent, HelpText }
